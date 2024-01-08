2bd4b0b19be09d402edfece2cdb29f7c6d8e4a62
Tony Nguyen <tonynguyeenn@gmail.com>
2024-01-07 23:49:29 -0800

    Updating code styles

--- src/app/workouts/page.jsx
+++ src/app/workouts/page.jsx
@@ -1,12 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function WorkoutsPage() {
   return (
     <div>
-        <Link href="/workouts/add" className='py-2 px-4 bg-white text-black rounded-full'>Add Workout</Link>
+      <Link
+        href="/workouts/add"
+        className="py-2 px-4 bg-white text-black rounded-full"
+      >
+        Add Workout
+      </Link>
     </div>
-  )
+  );
 }
 
-export default WorkoutsPage
+export default WorkoutsPage;
