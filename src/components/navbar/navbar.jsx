8f65735dd9d9e100c01b5335797dfd381a87c494
Tony <93945730+TonyNgyen@users.noreply.github.com>
2024-01-17 10:59:58 -0800

    Testing new SSH

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -10,7 +10,7 @@ async function Navbar() {
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
-        GymTracker Test
+        GymTracker
       </Link>
       <div>
         <Links session={session} />
