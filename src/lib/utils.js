1e848313af5e39b135cae078f665a5704744fda4
Tony Nguyen <tonynguyeenn@gmail.com>
2024-02-25 02:41:35 -0800

    Revert "Adding package"
    This reverts commit 31b44c793e6a25ba3a7e108383d5845f7ce4a6d8.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,17 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import mongoose from "mongoose";
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
+const connection = {};
+
+export const connectToDb = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log("Using existiting connection");
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
