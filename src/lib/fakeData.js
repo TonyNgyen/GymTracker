519623225b616e3064817be7a215cdc9f404c279
Tony Nguyen <tonynguyeenn@gmail.com>
2024-02-29 22:34:56 -0800

    Creating fake data to test ChartJS

--- src/lib/fakeData.js
+++ src/lib/fakeData.js
@@ -1,1 +1,16 @@
-// export const UserData
+export const UserData = {
+  workouts: [
+    {
+      name: "Abdul",
+      weights: [10, 18, 13, 12, 13, 15, 11, 10, 16, 16],
+    },
+    {
+      name: "Fletcher",
+      weights: [11, 15, 20, 19, 12, 16, 18, 15, 18, 12],
+    },
+    {
+      name: "Mckayla",
+      weights: [14, 13, 19, 20, 15, 14, 11, 11, 17, 15],
+    },
+  ],
+};
