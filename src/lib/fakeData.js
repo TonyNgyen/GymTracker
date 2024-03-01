c21bc1b763c4b4c2ea9ddc5bc245cdcc796559b3
Tony Nguyen <tonynguyeenn@gmail.com>
2024-03-01 00:57:32 -0800

    Updating fake data to better match database schema

--- src/lib/fakeData.js
+++ src/lib/fakeData.js
@@ -1,16 +1,95 @@
 export const UserData = {
-  workouts: [
+  result: [
+    ,
     {
-      name: "Abdul",
-      weights: [10, 18, 13, 12, 13, 15, 11, 10, 16, 16],
+      name: "Chest Press",
+      weights: [
+        {
+          date: "",
+          weight: 16,
+        },
+        {
+          date: "",
+          weight: 16,
+        },
+        {
+          date: "",
+          weight: 19,
+        },
+        {
+          date: "",
+          weight: 11,
+        },
+        {
+          date: "",
+          weight: 12,
+        },
+        {
+          date: "",
+          weight: 14,
+        },
+        {
+          date: "",
+          weight: 18,
+        },
+        {
+          date: "",
+          weight: 13,
+        },
+        {
+          date: "",
+          weight: 13,
+        },
+        {
+          date: "",
+          weight: 14,
+        },
+      ],
     },
     {
-      name: "Fletcher",
-      weights: [11, 15, 20, 19, 12, 16, 18, 15, 18, 12],
-    },
-    {
-      name: "Mckayla",
-      weights: [14, 13, 19, 20, 15, 14, 11, 11, 17, 15],
+      name: "Dips",
+      weights: [
+        {
+          date: "1/01/2024",
+          weight: 10,
+        },
+        {
+          date: "1/02/2024",
+          weight: 15,
+        },
+        {
+          date: "1/03/2024",
+          weight: 12,
+        },
+        {
+          date: "1/04/2024",
+          weight: 11,
+        },
+        {
+          date: "1/05/2024",
+          weight: 17,
+        },
+        {
+          date: "1/06/2024",
+          weight: 18,
+        },
+        {
+          date: "1/07/2024",
+          weight: 19,
+        },
+        {
+          date: "1/08/2024",
+          weight: 20,
+        },
+        {
+          date: "1/09/2024",
+          weight: 10,
+        },
+        {
+          date: "1/10/2024",
+          weight: 10,
+        },
+      ],
     },
   ],
 };
