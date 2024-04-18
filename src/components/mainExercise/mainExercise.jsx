fa6fe726e4d1f627c8ca8c2f7ab51de136d51782
Tony <93945730+TonyNgyen@users.noreply.github.com>
2024-04-17 22:28:01 -0700

    Adding files for refactoring

---
+++ src/components/mainExercise/mainExercise.jsx
@@ -0,0 +1,100 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { FaEdit, FaCheck } from "react-icons/fa";
+import styles from "./mainExercise.module.css";
+import { ExercisesContext } from "../workoutList/context";
+import { updateExercises } from "@/lib/actions";
+
+function MainExercise({ exercise }) {
+  const [exercisesContext, setExercisesContext] = useContext(ExercisesContext);
+  const [editToggle, setEditToggle] = useState(false);
+  const [weight, setWeight] = useState(exercise.weight);
+
+  useEffect(() => {
+    updateExercises(exercisesContext)
+  }, [exercisesContext])
+
+  const confirmEditSubmit = (e) => {
+    e.preventDefault();
+    setEditToggle(!editToggle);
+    let copyExercises = [];
+    for (let exerciseIndex in exercisesContext) {
+      if (exercisesContext[exerciseIndex].id == exercise.id) {
+        copyExercises.push({
+          id: exercise.id,
+          name: exercise.name,
+          sets: exercise.sets,
+          reps: exercise.reps,
+          weight: weight,
+        });
+      } else {
+        copyExercises.push(exercisesContext[exerciseIndex]);
+      }
+    }
+    setExercisesContext(copyExercises);
+  };
+
+  const confirmEditClick = () => {
+    setEditToggle(!editToggle);
+    let copyExercises = [];
+    for (let exercise in exercisesContext) {
+      if (exercisesContext[exercise].id == exercise.id) {
+        copyExercises.push({
+          id: exercise.id,
+          name: exercise.name,
+          sets: exercise.sets,
+          reps: exercise.reps,
+          weight: weight,
+        });
+      } else {
+        copyExercises.push(exercisesContext[exercise]);
+      }
+    }
+    setExercisesContext(copyExercises);
+  };
+  
+  return (
+    <div className={`${styles.workouts} text-xl`}>
+    <Button onClick={() => console.log(exercisesContext)}>Debug</Button>
+      <h1 className={styles.stats}>{exercise.name}</h1>
+      <h1 className={styles.stats}>{exercise.sets}</h1>
+      <h1 className={styles.stats}>{exercise.reps}</h1>
+      <h1 className={styles.stats}>
+        {!editToggle ? (
+          exercise.weight
+        ) : (
+          <form className="text-center" onSubmit={confirmEditSubmit}>
+            <input
+              type="number"
+              name="weight"
+              id=""
+              onChange={(e) => {
+                setWeight(e.target.value);
+              }}
+              placeholder="Weight"
+              value={weight}
+              className={styles.inputs}
+            />
+          </form>
+        )}
+      </h1>
+      {!editToggle ? (
+        <Button
+          className={`${styles.edit} bg-main hover:bg-main-foreground hover:text-foreground`}
+          onClick={() => setEditToggle(!editToggle)}
+        >
+          <FaEdit />
+        </Button>
+      ) : (
+        <Button
+          className={`${styles.edit} bg-greenConfirm hover:bg-greenConfirm-foreground hover:text-foreground`}
+          onClick={confirmEditClick}
+        >
+          <FaCheck />
+        </Button>
+      )}
+    </div>
+  );
+}
+
+export default MainExercise;
