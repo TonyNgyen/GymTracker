cb979d6d1aa7a6b525406da6d86c90568ba94ab7
Tony Nguyen <tonynguyeenn@gmail.com>
2024-08-04 03:57:00 -0700

    Working on workout history display

---
+++ src/components/historyExerciseGroup/historyExerciseGroup.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import styles from "./historyExerciseGroup.module.css";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+function HistoryExerciseGroup({ workout, exercise }) {
+  const [drop, setDrop] = useState(false);
+  return Object.keys(workout.exercises[exercise]).map((set) => (
+    (set == 1 || drop) && (<>
+      <tr
+        key={`${exercise}-${set}`}
+        className={`text-xl ${styles.tableRow} relative`}
+      >
+        <td className={`${styles.exerciseName}`}>
+          {workout.exercises[exercise][set].name}
+        </td>
+        <td className={styles.middle}>{set}</td>
+        <td className={styles.middle}>
+          {workout.exercises[exercise][set].reps}
+        </td>
+        <td className={styles.weight}>
+          {workout.exercises[exercise][set].weight}
+        </td>
+        {set == 1 ? <h2 className={`absolute ml-6 cursor-pointer text-2xl ${styles.dropButton}`} onClick={() => setDrop(!drop)}>{drop ? <IoIosArrowUp /> : <IoIosArrowDown />}</h2> : <></>}
+      </tr>
+    </>)
+  ));
+}
+
+export default HistoryExerciseGroup;
--- src/components/historyMainList/historyMainList.jsx
+++ src/components/historyMainList/historyMainList.jsx
@@ -1,33 +1,29 @@
 import React from "react";
 import styles from "./historyMainList.module.css";
+import HistoryExerciseGroup from "../historyExerciseGroup/historyExerciseGroup";
 
 function HistoryMainList({ workout, date }) {
   console.log(workout);
   return (
     <div className="text-center">
-      {date}
-      <table>
-        <thead>
-          <tr className="text-3xl">
-            <th className={styles.tableHeader}>Exercise</th>
-            <th className={styles.tableHeader}>Sets</th>
-            <th className={styles.tableHeader}>Reps</th>
-            <th className={styles.tableHeader}>Weight</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(workout.exercises).map((exercise) =>
-            Object.keys(workout.exercises[exercise]).map((set) => (
-              <tr key={`${exercise}-${set}`} className={`text-xl ${styles.tableRow}`}>
-                <td className={`${styles.exerciseName}`}>{workout.exercises[exercise][set].name}</td>
-                <td className={styles.tableData}>{set}</td>
-                <td className={styles.tableData}>{workout.exercises[exercise][set].reps}</td>
-                <td className={styles.tableData}>{workout.exercises[exercise][set].weight}</td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+      <h1>{date}</h1>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr className="text-3xl">
+              <th className={styles.tableHeader}>Exercise</th>
+              <th className={styles.tableHeader}>Set</th>
+              <th className={styles.tableHeader}>Reps</th>
+              <th className={styles.tableHeader}>Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(workout.exercises).map((exercise) =>
+              <HistoryExerciseGroup workout={workout} exercise={exercise} />
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
