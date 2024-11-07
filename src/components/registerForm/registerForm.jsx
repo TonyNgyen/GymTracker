9ea5ab84915bdf6377bd49b91c3d9ebe9f44147c
Tony Nguyen <tonynguyeenn@gmail.com>
2024-11-06 23:43:10 -0800

    Improving UI of sign up page

--- src/components/registerForm/registerForm.jsx
+++ src/components/registerForm/registerForm.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./registerForm.module.css";
 import { register } from "@/lib/actions";
 import { useFormState } from "react-dom";
@@ -8,9 +8,16 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { FaEye } from "react-icons/fa";
 
 function RegisterForm() {
   const [state, formAction] = useFormState(register, undefined);
+  const [passwordSetting, setPasswordSetting] = useState("password");
+  const togglePassword = () => {
+    passwordSetting == "password"
+      ? setPasswordSetting("text")
+      : setPasswordSetting("password");
+  };
 
   const router = useRouter();
 
@@ -19,21 +26,62 @@ function RegisterForm() {
     [state?.success, router]
   );
 
+  const passwordStyle = "flex bg-background items-center rounded-md pr-4";
+
   return (
-    <form action={formAction} className={styles.form}>
-      <input type="text" placeholder="username" name="username" id="" />
-      <input type="text" placeholder="email" name="email" id="" />
-      <input type="text" placeholder="password" name="password" id="" />
+    <form action={formAction} className={`${styles.form}`}>
+      <h1 className="text-xl font-semibold mb-[20px]">Sign Up</h1>
+      <label for="username" className="self-start mb-[5px] text-lg font-semibold">
+        Username
+      </label>
       <input
         type="text"
-        placeholder="password again"
-        name="passwordRepeat"
+        placeholder="Username"
+        name="username"
         id=""
+        className="mb-[15px] rounded-md"
       />
-      <Button>Register</Button>
+
+      <label for="email" className="self-start mb-[5px] text-lg font-semibold">
+        Email
+      </label>
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        id=""
+        className="mb-[15px] rounded-md"
+      />
+
+      <label for="username" className="self-start mb-[5px] text-lg font-semibold">
+        Password
+      </label>
+      <div className={`mb-[10px] ${passwordStyle}`}>
+        <input
+          type={passwordSetting}
+          placeholder="Password"
+          name="password"
+          id=""
+          className="w-full rounded-md"
+        />
+        <FaEye onClick={() => togglePassword()} className="cursor-pointer" />
+      </div>
+
+      <div className={`mb-[15px] ${passwordStyle}`}>
+        <input
+          type={passwordSetting}
+          placeholder="Password Again"
+          name="passwordRepeat"
+          id=""
+          className="w-full rounded-md"
+        />
+        <FaEye onClick={() => togglePassword()} className="cursor-pointer" />
+      </div>
+
+      <Button className="bg-main text-md font-semibold">Register</Button>
       {state?.error}
-      <Link href="/login">
-        Have an account? <b>Login</b>
+      <Link href="/login" className="mt-[20px]">
+        Have an account? <b className="text-main">Log In</b>
       </Link>
     </form>
   );
